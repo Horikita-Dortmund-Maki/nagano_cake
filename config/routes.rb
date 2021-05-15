28cf6940e002fff7bf93c5df40bee4eb40a97e39
Fikutaro <nomio9029@gmail>
2021-05-15 06:14:20 +0000

    add views gems

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,25 @@ Rails.application.routes.draw do
 
   root :to => "homes#top"
   get "home/about" =>"homes#about"
+  post 'orders/confirm(/:id)', to: 'orders#confirm'
+  get "/thanks" => "public/thanks#index"
+  post 'order_details/confirm(/:id)', to: 'order_datails#confirm'
+
+scope module: :public do
   resources :customers, only: [:show,:edit,:update,:create]
   resources :items, only: [:index,:show,:edit,:update]
   resources :cart_items, only: [:show, :destroy]
-  resources :order, only: [:show,:comfirm,:create,:index]
-  post 'orders/confirm(/:id)', to: 'orders#confirm'
-  get "/thanks" => "thanks#index"
-  resources :order_detail, only: [:show,:comfirm,:create,:index]
-  post 'order_details/confirm(/:id)', to: 'order_datails#confirm'
+  resources :orders, only: [:show,:comfirm,:create,:index]
+  resources :order_details, only: [:show,:comfirm,:create,:index]
   resources :addresses, only: [:index,:edit,:destroy,:create,:update]
+end
+  
   
   namespace :admin do
     resources :customers, only: [:index,:show,:edit,:update]
     resources :order_details, only: [:show]
     resources :orders, only: [:index,:edit,:update]
-    resources :genre, only: [:index,:edit,:update,:create]
+    resources :genres, only: [:index,:edit,:update,:create]
     resources :items, only: [:show,:edit,:new,:create]
   end
 
