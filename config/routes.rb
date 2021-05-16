a67032b9453fbf8a2e265f8bc45595edcda319d6
takapamu <takapamukyary0129@gmail.com>
2021-05-16 04:47:55 +0000

    header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admins, skip: :all
+  devise_scope :admin do
+    get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+    post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
+    delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'    
+  end
+  devise_for :customers, skip: :all
+    devise_scope :customer do
+    get 'customers/sign_in' => 'customers/sessions#new', as: 'new_customer_session'
+    post 'customers/sign_in' => 'customers/sessions#create', as: 'customer_session'
+    delete 'customers/sign_out' => 'customers/sessions#destroy', as: 'destroy_customer_session'
+    get 'customers/sign_up' => 'customers/registrations#new', as: 'new_customer_registration'
+    post 'customers' => 'customers/registrations#create', as: 'customer_registration'
+    get 'customers/password/new' => 'customers/passwords#new', as: 'new_customer_password'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => "homes#top"
@@ -9,14 +23,14 @@ Rails.application.routes.draw do
   get "/thanks" => "public/thanks#index"
   post 'order_details/confirm(/:id)', to: 'order_datails#confirm'
 
-scope module: :public do
-  resources :customers, only: [:show,:edit,:update,:create]
-  resources :items, only: [:index,:show,:edit,:update]
-  resources :cart_items, only: [:show, :destroy]
-  resources :orders, only: [:show,:comfirm,:create,:index]
-  resources :order_details, only: [:show,:comfirm,:create,:index]
-  resources :addresses, only: [:index,:edit,:destroy,:create,:update]
-end
+  scope module: :public do
+    resources :customers, only: [:show,:edit,:update,:create]
+    resources :items, only: [:index,:show,:edit,:update]
+    resources :cart_items, only: [:show, :destroy]
+    resources :orders, only: [:show,:comfirm,:create,:index]
+    resources :order_details, only: [:show,:comfirm,:create,:index]
+    resources :addresses, only: [:index,:edit,:destroy,:create,:update]
+  end
   
   
   namespace :admin do
