f879d45bd7f2bd6c14328a55f175654662aae44f
EC2 Default User <ec2-user@ip-172-31-58-43.ec2.internal>
2021-05-17 03:45:39 +0000

    Add

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@ Genre.create!(name: 'ジャンル')
 10.times do |n|
   Item.create!(name: "test#{n + 1}品目", introduction:"test!#{n + 1}品目", price:"#{n + 1}00", is_active: true, image_id: File.open("./app/assets/images/image#{n + 1}.png"), genre_id: 1)
 end
+
+Admin.create!(
+   name: 'testtest',
+   email: 'test@test.com',
+   password: 'testtest'
+  )
