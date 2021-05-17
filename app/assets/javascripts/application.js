7f30a6f31e459de4e9175722450ffccec1c47898
takapamu <takapamukyary0129@gmail.com>
2021-05-17 01:09:03 +0000

    ジャンル

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,12 +9,12 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
 //= require turbolinks
 //= require_tree .
