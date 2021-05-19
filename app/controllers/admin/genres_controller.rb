6a30da0e44069060c5fa48c47c6296273181246a
takapamu <takapamukyary0129@gmail.com>
2021-05-19 02:02:23 +0000

    CSS

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,35 +1,35 @@
 class Admin::GenresController < ApplicationController
-    def index
+  def index
      @genres = Genre.all
-      @genre = Genre.new
-    end
+     @genre = Genre.new
+  end
     
-    def edit
-        @genre = Genre.find(params[:id])
-    end
+  def edit
+      @genre = Genre.find(params[:id])
+  end
     
-    def create
-         @genre = Genre.new(genre_params)
-         @genre.save
-    end
+  def create
+      @genre = Genre.new(genre_params)
+      @genre.save
+  end
       
-    def update
-           @genre = Genre.find(params[:id])
-        if @genre.update(genres_params)
-           redirect_to admin_genres_path(@genre.id)
-        else
-           render "edit"
-        end
-    end 
+  def update
+       @genre = Genre.find(params[:id])
+    if @genre.update(genres_params)
+       redirect_to admin_genres_path(@genre.id)
+    else
+       render "edit"
+    end
+  end 
       
   private
       
-     def genre_params
-        params.permit(:name)
-     end
-     
-     def genres_params
-        params.require(:genre).permit(:name)
-     end
+  def genre_params
+    params.permit(:name)
+  end
+ 
+  def genres_params
+    params.require(:genre).p ermit(:name)
+  end
      
 end
