489454d7379e575d3c0709c7a5e8ef91cd165fd0
EC2 Default User <ec2-user@ip-172-31-58-43.ec2.internal>
2021-05-23 08:51:24 +0000

    successAjax

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,47 +1,53 @@
 class Public::CartItemsController < ApplicationController
-  before_action :setup_cart_item!, only: [:create, :update, :destroy]
+  # before_action :setup_cart_item!, only: [:create, :update, :destroy]
   before_action :authenticate_customer!
 
   # カート内アイテムの表示
   def show
-    @cart_items = current_customer.cart_items
-    # @total = CartItem
-    # @total = CartItem.all.sum(:price). #CartItemモデルには価格カラムがないためNG
-    # @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }　＃このやり方は無駄に難易度高い
-    @cart_item = CartItem.find_by(params[:id])
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    # @cart_items = current_customer.cart_items
+    # @cart_item = CartItem.find_by(params[:id])
   end
 
 
     # アイテムの追加
   def create
-     @cart_item = current_customer.cart_items.build(cart_item_params)
-      if  @cart_item.save
+    @cart_item = current_customer.cart_items.build(cart_item_params)
+    @cart_items = current_customer.cart_items.all
+    unless @cart_item.amount == nil
+      @cart_items.each do |cart_item|
+        if cart_item.item.id == @cart_item.item.id
+          new_amount = cart_item.amount + @cart_item.amount
+          cart_item.update_attribute(:amount, new_amount)
+          @cart_item.delete
           redirect_to cart_item_path(current_customer), success: "カートに商品を追加しました"
-      else
-           flash.now[:danger] = '商品を指定してください。'#課題点ここを渡された数字で判別できるようにしたい！
-           render template: "public/items/show"
+        end
       end
-    
+    else @cart_item.save
+      redirect_to request.referer, danger: "数量を指定してください。"
+      # flash[:danger] = '数量を指定してください。'#課題点ここを渡された数字で判別できるようにしたい！
+    end
+
   end
 
 
   # アイテムの更新
   def update
     @cart_item = CartItem.find(params[:id])
-    if @cart_item.update(cart_item_params) #ここで変更した数量を受け取って強制的に反映する
-      flash[:notice] = '数量が更新されました'
-    else
-      flash[:alert] = '数量の更新に失敗しました'
-    end
-    redirect_to cart_item_path(current_customer)
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    @cart_item.update(cart_item_params)  #ここで変更した数量を受け取って強制的に反映する
+    #   flash[:success] = "数量が更新されました"
+    # else
+    #   flash[:alert] = "数量の更新に失敗しました"
+    # end
+    # redirect_to cart_item_path(current_customer)
   end
 
   # アイテムの削除
   def destroy
-
     @cart_item = CartItem.find(params[:id])
-    @cart_item.destroy
     @cart_items = current_customer.cart_items
+    @cart_item.destroy
     # redirect_to cart_item_path(current_customer)
   end
 
@@ -63,8 +69,8 @@ class Public::CartItemsController < ApplicationController
     params.require(:cart_item).permit(:amount, :item_id, :customer_id)
   end
 
-  def setup_cart_item!
-    @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
-  end
+  # def setup_cart_item!
+  #   @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
+  # end
 
 end
