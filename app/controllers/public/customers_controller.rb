04b594c6c314bbe8e371132f505cf9d93e056c48
Fikutaro <nomio9029@gmail>
2021-05-23 02:31:04 +0000

    add

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,15 +1,15 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def show
     @customer = Customer.find(params[:id])
-    
+
   end
-  
+
   def edit
     @customer = Customer.find(params[:id])
-  end 
-  
+  end
+
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
@@ -19,21 +19,21 @@ class Public::CustomersController < ApplicationController
       render 'edit'
     end
   end
-  
+
     def unsubscribe
       @customer = Customer.find(params[:id])
     end
-  
+
     def withdraw
       @customer = Customer.find(params[:id])
       @customer.update(id_deleted: true)
       reset_session
       redirect_to root_path
     end
-  
+
     private
       def customer_params
         params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :id_deleted)
       end
-  end
+end
 
