0b5fc0728a095e710526d558fa1a89e7e5af3f09
EC2 Default User <ec2-user@ip-172-31-58-43.ec2.internal>
2021-05-24 06:45:22 +0000

    Add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,29 +4,29 @@ class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
    before_action :set_search
    add_flash_types :success, :info, :warning, :danger
-  
+
   def set_search
     @search = Item.ransack(params[:q])
     @items = @search.result
   end
-  
+
   # def after_sign_up_path_for(resource)
   #   items_path(resource)
   # end
 
-  
+
 
   protected
-  
+
   def after_sign_in_path_for(resource)
     case resource
     when Admin
       admin_items_path(resource)
     when Customer
-      items_path(resource)  
+      customer_path(resource)
     end
   end
-  
+
 
 
 
